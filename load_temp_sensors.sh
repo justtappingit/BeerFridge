c38d6d2828dc63898dae2de96830997ce24b0b19
Phillip Zimny <phillip.zimny@gmail.com>
2015-05-29 13:32:35 +0000

    Cleaning

--- load_temp_sensors.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-sudo modprobe w1-gpio
-
-sudo modprobe w1-therm
-sleep 1
-echo "Probes loaded: "
-ls /sys/bus/w1/devices | grep 28
--- test.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python
-
-import math
-
-i = 1.0
-while i < 30:
-	print str(i) +" = "+str(math.log(math.pow(i,2))) +" "+str(math.pow(1+math.log(i),2))
-	i+=1.0
-
